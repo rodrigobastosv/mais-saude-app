00494c4947fa135f340ac36e8164978efbeafaa3
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-12-16 09:58:59 -0300

    Some structure

--- lib/modules/home/blocs/home_bloc.dart
+++ lib/modules/home/blocs/home_bloc.dart
@@ -1,6 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeBloc extends Disposable {
+  int a;
+
+  HomeBloc() {
+    a = 5;
+  }
+
   @override
   void dispose() {
     print('HomeBloc foi disposado');
--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mais_saude_app/modules/home/blocs/home_bloc.dart';
 
-class HomePage extends StatelessWidget {
+import '../home_module.dart';
+
+class HomePage extends StatelessWidget with InjectMixin<HomeModule> {
   @override
   Widget build(BuildContext context) {
+    final bloc = get<HomeBloc>();
+    print(bloc.a);
     return Container(
       color: Colors.green,
     );
